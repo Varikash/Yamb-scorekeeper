de44922585dd0c0a05a565d313cb222734e07345
Nikolay Varikash <n.varicash@gmail.com>
2023-08-13 13:37:48 +0300

    refactoring

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,19 +7,20 @@ import SideField from "../SideField/SideField.jsx";
 function App() {
 
   return (
-    <section className='flex flex-col gap-y-2 max-w-xs mx-auto bg-[#ff7979] py-2 px-2'>
-      <Header />
-      <div className="flex gap-x-2">
+    <section className="w-full h-full bg-[#ff7979]">
+      <div className='flex flex-col gap-y-2 max-w-xs mx-auto bg-[#ff7979] py-2 px-2'>
+        <Header />
+        <div className="flex gap-x-2">
           <SideField />
-        <div className='flex flex-col gap-y-2'>
-          <SimpleDice />
-          <MinMaxDice />
-          <ComboDice />
+          <div className='flex flex-col gap-y-2'>
+            <SimpleDice />
+            <MinMaxDice />
+            <ComboDice />
+          </div>
         </div>
       </div>
-      
-      
     </section>
+    
   )
 }
 
